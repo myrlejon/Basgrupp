82f234bec15d21b041d538fae4dfcfcf51c600bb
Nils Odén <nisse757@hotmail.se>
2020-12-03 14:30:20 +0100

    Skapade en klass för medlemmar i gruppen.

--- Basgrupp/Program.cs
+++ Basgrupp/Program.cs
@@ -36,5 +36,46 @@ namespace Basgrupp
                 }
             }
         }
+
+        public static void Meny()
+        {
+            Console.WriteLine("Vad vill du göra\n1. Lista gruppens deltagare.\n2. Lista upp 10 generella detaljer om varje medlem.\n3. Ta bort en person.");
+
+        }
+    }
+
+    class Medlem
+    {
+        private string namn;
+        private int ålder;
+        private int födelsedag;
+        private string mat;
+        private string band;
+        private string film;
+        private string älskar;
+        private string hatar;
+        private string stjärntecken;
+        private string superkraft;
+        private string driv;
+
+        public Medlem()
+        {
+
+        }
+
+        public Medlem(string namn, int ålder, int födelsedag, string mat, string band, string film, string älskar, string hatar, string stjärntecken, string superkraft, string driv)
+        {
+            this.namn = namn;
+            this.ålder = ålder;
+            this.födelsedag = födelsedag;
+            this.mat = mat;
+            this.band = band;
+            this.film = film;
+            this.älskar = älskar;
+            this.hatar = hatar;
+            this.stjärntecken = stjärntecken;
+            this.superkraft = superkraft;
+            this.driv = driv;
+        }
     }
 }
